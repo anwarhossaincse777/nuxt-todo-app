60872ab1e168c316eaf4de30249da8a77c619110
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-10-04 08:51:59 +0600

    all-pro

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppHeader from "~/components/AppHeader.vue";
+import AppFooter from "~/components/AppFooter.vue";
+
+
+
+</script>
+
+
+
+<template>
+  <div>
+    <AppHeader />
+    <router-view></router-view>
+    <AppFooter />
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
